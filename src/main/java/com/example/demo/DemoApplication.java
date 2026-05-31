8a128d3532ff413f0cbb7c78e14684a201fa86e6
Adrian Rey <rey.code@proton.me>
2026-05-31 02:19:51 +0200

    Cambiado metodo main (el maven no le gusta que el main no lleve static, aunque es feature de java 26)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
